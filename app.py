5a9b06dd0323ad6486b9c96735769749b3451bf9
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-05-01 13:45:48 +0100

    Remove main.css file and update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from PIL import Image
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 
 
@@ -32,8 +31,6 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 
